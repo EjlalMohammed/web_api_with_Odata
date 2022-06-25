0b8f7defbcdd5e746780912e419edb3f06efa019
ejomo <ejlalmohammed3@gmail.com>
2022-06-25 22:06:36 +0300

    add author model and controller

---
+++ WebApplicationapi/Controllers/AuthorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationapi.modesl;
+
+namespace WebApplicationapi.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        public static List<Author> Authors = new List<Author>()
+        {
+             new Author(){AuthorId=10, AuthorName="EJLAL",AuthorLevel="1"},
+                new Author(){AuthorId=20, AuthorName="OLA",AuthorLevel="1"},
+        };
+
+
+        [HttpGet]
+        public List<Author> GetAll()
+        {
+            return Authors;
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetStudentById(int Id)
+        {
+            var CurAuthor = Authors.Where(x => x.AuthorId == Id).FirstOrDefault();
+            if (CurAuthor == null)
+            {
+                return NotFound(new
+                {
+                    ErrorCode = 501,
+                    ErrorMessage = "author not found"
+
+                });
+            }
+            return Ok(CurAuthor);
+
+        }
+        [HttpDelete("{AuthorId}")]
+        public IActionResult deleteauther(int AuthorId)
+        {
+            var CurAuthor = Authors.Where(x => x.AuthorId == AuthorId).SingleOrDefault();
+            if (CurAuthor == null)
+            {
+                return NotFound(new
+                {
+                    ErrorCode = 501,
+                    ErrorMessage = "author not found"
+
+                });
+            }
+            Authors.Remove(CurAuthor);
+            return NoContent();
+        }
+    }
+
+}
+
---
+++ WebApplicationapi/modesl/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationapi.modesl
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string? AuthorName { get; set; }
+        public string? AuthorLevel { get; set; }
+    }
+}
